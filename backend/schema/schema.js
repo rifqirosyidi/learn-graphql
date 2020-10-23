48a04d943ca9327b9143aae3145cabe69fc500bc
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-10-23 14:57:24 +0700

    mutation create

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const graphql = require("graphql");
 const _ = require('lodash')
 const mongoose = require('mongoose')
-const book = require('../models/book')
-const author = require('../models/author')
+const Book = require('../models/book')
+const Author = require('../models/author')
 
 const {
    GraphQLObjectType,
@@ -85,6 +85,45 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        })
+        return author.save()
+      }
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId
+        })
+
+        return book.save()
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation
 });
